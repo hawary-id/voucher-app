adbaa67a7abe082660d34701c0b99c0844770b86
Syamsul Hadi <syamsul.hadi@praweda.id>
2026-06-27 22:49:57 +0700

    chore: conditionally disable wayfinder plugin during Vercel builds

--- vite.config.ts
+++ vite.config.ts
@@ -24,9 +24,11 @@ export default defineConfig({
             },
         }),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-            command: 'C:/laragon/bin/php/php-8.3.29-nts-Win32-vs16-x64/php.exe artisan wayfinder:generate',
-        }),
+        ...(process.env.VERCEL ? [] : [
+            wayfinder({
+                formVariants: true,
+                command: 'C:/laragon/bin/php/php-8.3.29-nts-Win32-vs16-x64/php.exe artisan wayfinder:generate',
+            })
+        ]),
     ],
 });
